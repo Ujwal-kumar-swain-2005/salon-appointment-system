333adf3871e9cedddeda8d5f2ee67a976a7a5067
Ujwal-kumar-swain-2005 <swainujwalkumar@gmail.com>
2026-01-06 16:15:58 +0530

    crud User operation

---
+++ Backend/user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.example.user_service.controller;
+
+import com.example.user_service.modal.User;
+import com.example.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/api/users")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/api/users")
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/api/users/{userId}")
+    public User getUserById(@PathVariable("userId") Long id) throws Exception {
+        Optional<User> optional = userRepository.findById(id);
+        if (optional.isPresent())
+            return optional.get();
+
+        throw new Exception("User not found.");
+    }
+
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable Long id) throws Exception {
+        Optional<User> optional = userRepository.findById(id);
+        if (optional.isEmpty()) {
+            throw new Exception("User not found with ID " + id);
+        }
+        var existingUser = optional.get();
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPhone(user.getPhone());
+        existingUser.setRole(user.getRole());
+
+        return userRepository.save(existingUser);
+    }
+
+    @DeleteMapping("/api/users/{id}")
+    public String deleteUserById(@PathVariable Long id) throws Exception {
+        Optional<User> optional = userRepository.findById(id);
+        if (optional.isEmpty()) {
+            throw new Exception("User not exits with ID " + id);
+        }
+
+        userRepository.deleteById(id);
+        return "User deleted";
+    }
+}
